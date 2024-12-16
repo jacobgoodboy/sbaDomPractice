46f375263587967091c3df55638125f2006c851a
jacobgoodboy <jacobgoodboy9@gmail.com>
2024-12-15 20:05:39 -0600

    added app.js functions to add listeners

---
+++ Desktop/perscholas/sba2/app.js
@@ -0,0 +1,8 @@
+const contacts=[{
+    firstname:'nane',
+    lastname:'mane',
+    phonenumber:'312459998'
+    
+}]
+const fname=document.getElementById('fname')
+const lname=document.getElementById('lname')
