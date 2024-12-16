5cec9837bf9ac15926033a1893d1be9412cb7cd4
jacobgoodboy <jacobgoodboy9@gmail.com>
2024-12-15 21:39:23 -0600

    search and add function works well, just need to add validations

--- Desktop/perscholas/sba2/app.js
+++ Desktop/perscholas/sba2/app.js
@@ -1,24 +1,24 @@
-const contacts=[{
-    firstname:'john',
-    lastname:'doe',
-    phonenumber:'3124599999'
-    
+const contacts = [{
+    firstname: 'john',
+    lastname: 'doe',
+    phonenumber: '3124599999'
+
 },
 {
-    firstname:'jane',
-    lastname:'smith',
-    phonenumber:3124449999
+    firstname: 'jane',
+    lastname: 'smith',
+    phonenumber: 3124449999
 }]
 
-const results=document.querySelector('.results')
+const results = document.querySelector('.results')
 
 //declaring the buttons
 
-const fname=document.getElementById('fname')
-const lname=document.getElementById('lname')
-const addbtn=document.getElementById('addbtn')
-const searchbtn=document.getElementById('searchbtn')
-const addfield=document.getElementById('addnumberfield')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const addbtn = document.getElementById('addbtn')
+const searchbtn = document.getElementById('searchbtn')
+const addfield = document.getElementById('addnumberfield')
 //declaring some listeners
 // fname.addEventListener('change', function (){
 //     console.log(' input logging fname')
@@ -33,58 +33,61 @@ const addfield=document.getElementById('addnumberfield')
 
 //add button implementation
 
-addbtn.addEventListener('click', function(){
-if(lname.value&&fname.value){
-    //added the input
-    const inp=document.createElement('input')
-    inp.placeholder='Enter the phone number:'
-    inp.type='tel'
-    addfield.appendChild(inp)
-    //added confirm button
-    const confirm=document.createElement('button')
-    confirm.textContent='confirm'
-    addfield.append(confirm)
-    //confirm button
-    confirm.addEventListener('click', function(){
-        const newcontact={
-            firstname:fname.value,
-            lastname:lname.value,
-            phonenumber:inp.value
-        }
-        contacts.push(newcontact)
+addbtn.addEventListener('click', function () {
+    if (lname.value && fname.value) {
+        //added the input
+        const inp = document.createElement('input')
+        inp.placeholder = 'Enter the phone number:'
+        inp.type = 'tel'
+        addfield.appendChild(inp)
+        //added confirm button
+        const confirm = document.createElement('button')
+        confirm.textContent = 'confirm'
+        addfield.append(confirm)
+        //confirm button
+        confirm.addEventListener('click', function () {
+            const newcontact = {
+                firstname: fname.value,
+                lastname: lname.value,
+                phonenumber: inp.value
+            }
+            contacts.push(newcontact)
+            fname.value = ''
+            lname.value = ''
+            addfield.innerHTML = ''
+        })
 
-        addfield.innerHTML=''
-    })
-    
 
-}else{
-    alert('Please enter first and Last name!')
-}
+    } else {
+        alert('Please enter first and Last name!')
+    }
 
 })
 
 
 //search btn implementation works
-searchbtn.addEventListener('click', function(){
+searchbtn.addEventListener('click', function () {
 
-    if(lname.value&&fname.value){
-        for(contact of contacts){
-            if(fname.value == contact.firstname && lname.value ==contact.lastname){
+    if (lname.value && fname.value) {
+        for (contact of contacts) {
+            if (fname.value == contact.firstname && lname.value == contact.lastname) {
                 results.removeChild(results.firstChild)
-                const result=document.createElement('div')
-                
-                const searchresult=`${fname.value} ${lname.value} ${contact.phonenumber}`
-                result.textContent=searchresult
+                const result = document.createElement('div')
+
+                const searchresult = `${fname.value} ${lname.value} ${contact.phonenumber}`
+                result.textContent = searchresult
                 results.appendChild(result)
-                lname.value=''
-                fname.value=''
-                return
+                lname.value = ''
+                fname.value = ''
+                break
             }
-            else{
-                alert('Contact not found!')
-            }
+
         }
-    }else{
+    } else if (lname.value && fname.value) {
+        alert('Contact not found!')
+    }
+
+    else {
         alert('Please enter first and Last name!')
     }
 
